e4a1a6746c5dfe4fbd6244ae0025f6afd40d84a9
JoacyMS <joacymsilva@gmail.com>
2020-04-01 19:31:20 -0300

    Seção serviços

--- portifolio/src/main.js
+++ portifolio/src/main.js
@@ -19,6 +19,7 @@ class App {
             'Vuejs'
         ];
 
+        this.techsListEl = document.getElementById('techs-list');
         this.listEl = document.getElementById('repo-list');
 
         this.getUser();
